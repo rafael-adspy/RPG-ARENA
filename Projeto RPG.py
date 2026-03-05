aa45acb8707a55e163a8f14d6664facfcb22f2c6
rafael-adspy <rafael145007@icloud.com>
2026-03-04 23:58:36 -0300

    RPG ARENA v0.1.2
    Iniciei os primeiros menus de batalha.

--- Projeto RPG.py
+++ Projeto RPG.py
@@ -1,4 +1,5 @@
 from time import sleep
+from random import randint
 def menu1():
     return('''
     [1] PERSONAGENS
@@ -16,8 +17,62 @@ def menu_char():
     ''')
 
 
-def ataque(a, b):
+def ataque_jogador(a, b):
     return a - b
+
+
+def ataque_maquina():
+    return randint(1, 2)
+
+
+def vida_jogador(a, b):
+    return a - b
+
+
+def vida_maquina(a, b):
+    return a - b
+
+
+def menu_batalhas():
+
+    while True:
+        print('''\n[1] Mago Negro
+        [2] Ninja
+        [3] Guerreiro''')
+        try:
+            user = int(input('Escolha um Personagem: '))
+            match user:
+                case 1:
+                    print('Ótima Opção, escolheu o Mago Negro.')
+                    break
+                case 2:
+                    print('Nossa escolha magnifica, escolheu o Ninja.')
+                    break
+                case 3:
+                    print('Você é corajoso, escolheu o Guerreiro.')
+                    break
+                case _:
+                    print('Escolha Personagens Válidos')
+        except ValueError:
+            print('Escolha uma opção válida: ')
+
+
+
+def only_personagem():
+    return {
+        "1": { "nome": "Mago Negro", "vida": 80, "ataque": 20, "defesa": 5},
+        "2": { "nome": "Ninja", "vida": 90, "ataque": 15, "defesa": 7},
+        "3": { "nome": "Guerreiro", "vida": 110, "ataque": 15, "defesa": 12},
+
+    }
+personagem = only_personagem()
+
+
+
+def batalhas_menu():
+    print('''[1] Atacar
+    [2] Defender
+    [3] Surrender ''')
 
 
 print(f'{"RPG-ARENA":=^20}')
@@ -31,13 +86,18 @@ print(f'{"RPG-ARENA":=^32}')
 print('AGUARDE...')
 r1 = '='
 for w in range(1, 19):
-    sleep(0.3)
+    sleep(0.2)
     print(r1, end=' ')
 while True:
     print(menu1())
-    op1 = (input('Selecione a opção: '))
+    op_menu = (input('Selecione a opção: '))
 
-    if op1 == '1':
+    if op_menu == '3':
+        print('Saindo do jogo...')
+        sleep(1)
+        break
+
+    if op_menu == '1':
         print(menu_char())
         char = (input('Selecione o personagem: '))
         if char == '1':
@@ -46,11 +106,12 @@ while True:
             print('''
             Mago Negro – Mestre das artes sombrias, usa feitiços poderosos para atacar à distância.
             Frágil no corpo, mas mortal com sua magia.
-            Atributo	Valor	Observação
-            Vida	80	Frágil no corpo, fácil de ser atingido
-            Ataque	20	Poderoso com magia à distância
-            Defesa	5	Baixa resistência física
+            Atributo     | Valor    | Observação
+            Vida         | 80	    | Frágil no corpo, fácil de ser atingido
+            Ataque       | 20	    | Poderoso com magia à distância
+            Defesa       | 5	    | Baixa resistência física
             ''')
+
         elif char == '2':
             print(f'NINJA...')
             sleep(2)
@@ -58,11 +119,12 @@ while True:
             Ninja – Mestre da furtividade e agilidade.
             Ataques rápidos e esquiva alta, ideal para jogadores que preferem movimentação estratégica.
             Ninja — Atributos iniciais
-            Atributo	Valor	Observação
-            Vida	90	Média, mais resistente que o mago
-            Ataque	15	Ataque rápido, não tão forte quanto o mago
-            Defesa	7	Média, consegue suportar alguns ataques
+            Atributo	 | Valor   | Observação
+            Vida         | 90	   | Média, mais resistente que o mago
+            Ataque       | 15	   | Ataque rápido, não tão forte quanto o mago
+            Defesa       | 7	   | Média, consegue suportar alguns ataques
             ''')
+
         elif char == '3':
             print('GUERREIRO')
             print(f'''
@@ -75,7 +137,7 @@ while True:
 
         else:
             print('Opção Invalida')
-    elif op1 == '2':
+    elif op_menu == '2':
         print('CARREGANDO...')
         sleep(2)
         print(f'''
@@ -113,13 +175,7 @@ while True:
         proporcionando diferentes estilos de jogo.
         {"=" * 70}
         ''')
-    elif op1 == '3':
-        print('Saindo do jogo...')
-        sleep(2)
-        break
+
     else:
         print('Opção invalida')
-        
 
-
-        
