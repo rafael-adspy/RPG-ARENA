03cab0a2c182524cdcf1c1bded0f895f6deadf04
rafael-adspy <rafael145007@icloud.com>
2026-03-05 22:29:27 -0300

    RPG ARENA v.0.1.3
    O sistema de batalha do RPG-ARENA foi temporariamente
    simplificado para apenas duas ações:
    
    [1] Atacar
    [2] Curar
    
    Durante o desenvolvimento, percebi que a versão anterior
    do sistema de combate estava ficando complexa demais para
    o estágio atual do projeto, o que acabou gerando algumas
    dificuldades na implementação.
    
    Para manter o jogo funcional e continuar evoluindo no
    aprendizado de programação, a batalha foi simplificada
    temporariamente.
    
    Em futuras atualizações o sistema de combate será otimizado
    e novas mecânicas serão adicionadas, como:
    
    Defesa
    
    Habilidades especiais
    
    Sistema de combate mais estratégico

--- Projeto RPG.py
+++ Projeto RPG.py
@@ -1,10 +1,56 @@
 from time import sleep
 from random import randint
+
+#ef only_personagem():  Essa Funçao Será Adicionada Em Breve!!!
+#return {
+#       "1": { "nome": "Mago Negro", "vida": 80, "ataque": 20, "defesa": 5},
+#      "2": { "nome": "Ninja", "vida": 90, "ataque": 15, "defesa": 7},
+#      "3": { "nome": "Guerreiro", "vida": 110, "ataque": 15, "defesa": 12},
+
+ #  }
+
+#personagens = only_personagem()
+
+def aviso():
+    print('''==============================================================
+AVISO DE DESENVOLVIMENTO
+========================
+
+O sistema de batalha do RPG-ARENA foi temporariamente
+simplificado para apenas duas ações:
+
+[1] Atacar
+[2] Curar
+
+Durante o desenvolvimento, percebi que a versão anterior
+do sistema de combate estava ficando complexa demais para
+o estágio atual do projeto, o que acabou gerando algumas
+dificuldades na implementação.
+
+Para manter o jogo funcional e continuar evoluindo no
+aprendizado de programação, a batalha foi simplificada
+temporariamente.
+
+Em futuras atualizações o sistema de combate será otimizado
+e novas mecânicas serão adicionadas, como:
+
+* Defesa
+* Habilidades especiais
+* Sistema de combate mais estratégico
+
+Obrigado por testar o RPG-ARENA!
+Este projeto continuará evoluindo conforme meu aprendizado
+em Python avança.
+=================
+''')
+
+
 def menu1():
     return('''
     [1] PERSONAGENS
     [2] SOBRE
-    [3] SAIR 
+    [3] BATALHAS
+    [4] SAIR 
     ''')
 
 
@@ -15,22 +61,6 @@ def menu_char():
     [2] Ninja
     [3] Guerreiro
     ''')
-
-
-def ataque_jogador(a, b):
-    return a - b
-
-
-def ataque_maquina():
-    return randint(1, 2)
-
-
-def vida_jogador(a, b):
-    return a - b
-
-
-def vida_maquina(a, b):
-    return a - b
 
 
 def menu_batalhas():
@@ -58,21 +88,24 @@ def menu_batalhas():
 
 
 
-def only_personagem():
-    return {
-        "1": { "nome": "Mago Negro", "vida": 80, "ataque": 20, "defesa": 5},
-        "2": { "nome": "Ninja", "vida": 90, "ataque": 15, "defesa": 7},
-        "3": { "nome": "Guerreiro", "vida": 110, "ataque": 15, "defesa": 12},
-
-    }
-personagem = only_personagem()
-
-
-
 def batalhas_menu():
-    print('''[1] Atacar
-    [2] Defender
-    [3] Surrender ''')
+    return print('''[1] Atacar
+    [2] Curar
+    ''')
+
+
+def batalhas_1():
+    print(f'\n{"1 ROUND":=^20}')
+    efeito_2 = '= = = = = YOU vs CPU= = = = ='
+    for letra in efeito_2:
+        sleep(1)
+        print(letra, end=' ')
+    print()
+
+
+def ataque_jogador(a, b):
+    return a - b
+
 
 
 print(f'{"RPG-ARENA":=^20}')
@@ -92,7 +125,7 @@ while True:
     print(menu1())
     op_menu = (input('Selecione a opção: '))
 
-    if op_menu == '3':
+    if op_menu == '4':
         print('Saindo do jogo...')
         sleep(1)
         break
@@ -133,7 +166,8 @@ while True:
             Vida          | 110   | Maior resistência, suporta mais ataques
             Ataque        | 15    | Dano médio, confiável em combates diretos
             Defesa        | 12    | Forte, reduz consideravelmente o dano recebido
-            ''')
+           ''')
+
 
         else:
             print('Opção Invalida')
@@ -145,18 +179,50 @@ while True:
         {"SOBRE O JOGO - RPG ARENA":^70}
         {"=" * 70}
     
-        RPG-ARENA é um jogo desenvolvido em Python com foco em
-        lógica de programação e evolução contínua.
-    
-        O projeto foi criado com o objetivo de praticar conceitos
-        fundamentais como:
-        - Estruturas condicionais
-        - Loops
-        - Listas
-        - Funções
-        - Organização de código
-        O jogo será modificado sempre que eu evoluir meu aprendizado,
-        e assim irei adicionar novas funções ao jogo.
+        RPG-ARENA é um jogo desenvolvido em Python com foco em lógica de
+programação e evolução contínua.
+
+O projeto foi criado com o objetivo de praticar conceitos
+fundamentais como:
+
+Estruturas condicionais
+
+Loops
+
+Listas e dicionários
+
+Funções
+
+Organização de código
+
+O jogo será modificado sempre que eu evoluir meu aprendizado,
+e assim novas funcionalidades serão adicionadas ao projeto.
+
+Durante o desenvolvimento percebi que algumas ideias iniciais
+estavam deixando o sistema de batalha mais complexo do que o
+necessário para o estágio atual do projeto.
+
+Por esse motivo, decidi simplificar temporariamente o sistema
+de combate, utilizando uma mecânica básica de ataque e cura,
+para garantir que o jogo funcione corretamente enquanto continuo
+evoluindo meus conhecimentos em programação.
+
+No futuro, pretendo adicionar novamente mecânicas mais avançadas
+como defesa, habilidades especiais e melhorias no sistema de
+batalha.
+
+🧙‍♂️ MAGO NEGRO
+Alto poder mágico e grande capacidade ofensiva, porém com
+menor resistência física.
+
+🥷 NINJA
+Especialista em agilidade e ataques rápidos.
+
+⚔️ GUERREIRO
+Grande resistência e equilíbrio entre ataque e sobrevivência.
+
+Cada personagem possui atributos próprios de vida e ataque,
+proporcionando diferentes estilos de jogo.
     
         {"PERSONAGENS INICIAIS":^70}
         {"-" * 70}
@@ -176,6 +242,42 @@ while True:
         {"=" * 70}
         ''')
 
-    else:
-        print('Opção invalida')
+
+    elif op_menu == '3':
+        eu = 100
+        inimigo = 100
+
+        while eu > 0 and inimigo > 0:
+            print(f'Minha Vida {eu}')
+            print(f'Vida do Inimigo {inimigo}')
+
+            print('''
+            [1] Atacar
+            [2] Curar''')
+
+            acao = str(input('Selecione uma opção:'))
+            if acao == '1':
+                dano = randint(10, 20)
+                inimigo -= dano
+                print(f'Você Causou {dano} no Inimigo')
+
+            elif acao == '2':
+                cura = randint(7, 25)
+                eu += cura
+                print(f'Você Recuperou {cura} de Vida')
+
+            else:
+                print('Opção Invalida')
+                continue
+
+            if inimigo <=0:
+                print('Parabéns Você Ganhou')
+                break
+
+            dano_inimigo = randint(10, 20)
+            eu -= dano_inimigo
+            print(f'Inimigo Causou {dano_inimigo} em Você!')
+
+            if eu <= 0:
+                    print('Você Perdeu kkk Tente Outra Vez!!!')
 
